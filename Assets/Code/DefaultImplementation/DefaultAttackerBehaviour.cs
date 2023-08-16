a200c71b8572f942ad91d8a0322922d4b54b6ff1
Laszlo Kocsis <kocsislaci95@gmail.com>
2023-08-16 10:14:59 +0200

    feat(player): added cooldown mechanism to attackerBehaviour

--- Assets/Code/DefaultImplementation/DefaultAttackerBehaviour.cs
+++ Assets/Code/DefaultImplementation/DefaultAttackerBehaviour.cs
@@ -1,16 +1,15 @@
 using Interfaces;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 namespace DefaultImplementation
 {
     public class DefaultAttackerBehaviour: AttackerBehaviour
     {
-        public override void InvokeCombatAction1(InputAction.CallbackContext context)
+        public override void CombatAction1()
         {
             Debug.Log("Invoked Combat action 1");
         }
-        public override void InvokeCombatAction2(InputAction.CallbackContext context)
+        public override void CombatAction2()
         {
             Debug.Log("Invoked Combat action 2");
         }
--- Assets/Code/Interfaces/AttackerBehaviour.cs
+++ Assets/Code/Interfaces/AttackerBehaviour.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,12 +6,45 @@ namespace Interfaces
 {
     public abstract class AttackerBehaviour: MonoBehaviour
     {
-        // TODO cooldown
-        // TODO cooldown counter coroutines
+        private float _cooldownDuration1 = 1f;
+        private float _cooldownDuration2 = .5f;
+        private bool _isCooldown1 = false;
+        private bool _isCooldown2 = false;
+        
+        IEnumerator StartCooldown1()
+        {
+            _isCooldown1 = true;
+            yield return new WaitForSeconds(_cooldownDuration1);
+            _isCooldown1 = false;
+        }
+        
+        IEnumerator StartCooldown2()
+        {
+            _isCooldown2 = true;
+            yield return new WaitForSeconds(_cooldownDuration2);
+            _isCooldown2 = false;
+        }
 
+        public void InvokeCombatAction1(InputAction.CallbackContext context)
+        {
+            if (!_isCooldown1)
+            {
+                StartCoroutine(StartCooldown1());
+                CombatAction1();
+            }
+        }
 
-        public abstract void InvokeCombatAction1(InputAction.CallbackContext context);
+        public abstract void CombatAction1();
 
-        public abstract void InvokeCombatAction2(InputAction.CallbackContext context);
+        public void InvokeCombatAction2(InputAction.CallbackContext context)
+        {
+            if (!_isCooldown2)
+            {
+                StartCoroutine(StartCooldown2());
+                CombatAction2();
+            }
+        }
+        public abstract void CombatAction2();
+
     }
 }
